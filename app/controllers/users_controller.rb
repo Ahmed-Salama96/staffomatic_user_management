fe50e8d22115e0541ca0af2d2dd69b23c9f0b4f2
Ahmed <a.salamaa595@gmail.com>
2021-07-02 02:10:48 +0200

    rename @user to @target_user because it is defined in application_controller as the current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   before_action :validate_already_unarchived, only: %i[unarchive]
 
   def index
-    @users = case params[:status]
+    @target_users = case params[:status]
              when 'deleted'
                User.only_deleted
              when 'archived'
@@ -18,35 +18,35 @@ class UsersController < ApplicationController
                User.all
              end
 
-    render jsonapi: @users
+    render jsonapi: @target_users
   end
 
   def destroy
     # Remove the user
-    render json: { error: @user.errors.full_messages.join(', ') }, status: :bad_request unless @user.destroy
+    render json: { error: @target_user.errors.full_messages.join(', ') }, status: :bad_request unless @target_user.destroy
 
     # Send him email about this changes
-    UserMailer.notify_user_deleted(@user).deliver_now
+    UserMailer.notify_user_deleted(@target_user).deliver_now
 
     render json: { message: 'user deleted successfully' }, status: :ok
   end
 
   def archive
     # Archive the user
-    render json: { error: @user.errors.full_messages.join(', ') }, status: :bad_request unless @user.archive!
+    render json: { error: @target_user.errors.full_messages.join(', ') }, status: :bad_request unless @target_user.archive!
 
     # Send him email about this changes
-    UserMailer.notify_user_archived(@user).deliver_now
+    UserMailer.notify_user_archived(@target_user).deliver_now
 
     render json: { message: 'user archived successfully' }, status: :ok
   end
 
   def unarchive
     # Usnarchive the user
-    render json: { error: @user.errors.full_messages.join(', ') }, status: :bad_request unless @user.unarchive!
+    render json: { error: @target_user.errors.full_messages.join(', ') }, status: :bad_request unless @target_user.unarchive!
 
     # Send him email about this changes
-    UserMailer.notify_user_unarchived(@user).deliver_now
+    UserMailer.notify_user_unarchived(@target_user).deliver_now
 
     render json: { message: 'user unarchived successfully' }, status: :ok
   end
@@ -54,20 +54,20 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find_by(id: params[:id])
+    @target_user = User.find_by(id: params[:id])
   end
 
   def validate_user
-    render json: { error: 'record not found' }, status: :not_found unless @user.present?
+    return render json: { error: 'record not found' }, status: :not_found unless @target_user.present?
     render json: { error: 'you cannot delete, archive, or un-archive yourself' },
-           status: :not_found if @user.id == current_user.id
+           status: :not_found if @target_user.id == current_user.id
   end
 
   def validate_already_archived
-    render json: { error: 'user already archived' }, status: :bad_request if @user.is_archived?
+    render json: { error: 'user already archived' }, status: :bad_request if @target_user.is_archived?
   end
 
   def validate_already_unarchived
-    render json: { error: 'user already unarchived' }, status: :bad_request unless @user.is_archived?
+    render json: { error: 'user already unarchived' }, status: :bad_request unless @target_user.is_archived?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resource :signup, only: %i[create]
   resources :authentications, only: %i[create]
-  resources :users, only: %i[index destroy] do
-    post 'archive', action: :archive
-    post 'unarchive', action: :unarchive
+  resources :users, only: %i[index destroy]
+
+  scope(controller: 'users') do
+    post 'users/archive/:id', action: :archive, as: :archive_user
+    post 'users/unarchive/:id', action: :unarchive, as: :unarchive_user
   end
 end
